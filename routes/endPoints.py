8d762fa0d7d3ce5756190554480509473cc7b783
Vedika Saini <vedikasaini10@gmail.com>
2023-09-23 20:46:40 +0800

    final lazy developer

--- routes/endPoints.py
+++ routes/endPoints.py
@@ -9,35 +9,37 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-def getNextProbableWords(classes: List[Dict],
-                         statements: List[str]) -> Dict[str, List[str]]:
-    # Fill in your solution here and return the correct output based on the given input
+from typing import Dict, List
+
+def getNextProbableWords(classes: List[Dict], statements: List[str]) -> Dict[str, List[str]]:
     result = {}
     cdict = {k: v for ci in classes for k, v in ci.items()}
+
     for x in statements:
         result[x] = []
         l = x.split('.')
         first_sub = l[0]
-        second_sub = l[1]
-        if (first_sub in cdict.keys()):
-            if type(cdict[first_sub]) == dict and second_sub != "":
+        second_sub = l[1] if len(l) > 1 else ""
+
+        if first_sub in cdict:
+            if isinstance(cdict[first_sub], dict) and second_sub != "":
                 klist = cdict[first_sub].keys()
                 for j in klist:
-                    if (j.find(second_sub) != -1):
-                        if (cdict[first_sub][j][:4] != 'List'):
-                            result[x] += [j]
+                    if j.find(second_sub) != -1 and not cdict[first_sub][j].startswith('List'):
+                        result[x].append(j)
             else:
                 klist = cdict[first_sub]
                 for j in klist:
-                    if (j.find(second_sub) != -1):
-                        result[x] += [j]
+                    if j.find(second_sub) != -1:
+                        result[x].append(j)
+
         result[x].sort()
         if result[x] == []:
             result[x] = ['']
         final = result[x]
-        if (len(final) > 5):
+        if len(final) > 5:
             result[x] = final[:5]
-    # print(result)
+
     return result
 
 
