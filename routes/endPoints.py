3b69443344e4e866d7a42ba1ecda792b5df2c694
shafirasya <shafira.asya2@gmail.com>
2023-09-24 06:31:05 +0800

    add chinese wall & pie chart

--- routes/endPoints.py
+++ routes/endPoints.py
@@ -2,6 +2,7 @@ import logging
 import socket
 from routes import app
 from flask import Flask, request, jsonify, json
+import math
 from typing import Dict, List
 from functools import lru_cache
 
@@ -215,7 +216,7 @@ class Passenger:
 def prioritizeQueue(listPassengers, cutoff_time):
     # print('list: ', listPassengers)
     sortedPassengers = [
-        passenger for passenger in listPassengers if passenger.askTimeToDeparture() <= cutoff_time]
+        passenger for passenger in listPassengers if passenger.askTimeToDeparture() >= cutoff_time]
     sortedPassengers = sorted(
         sortedPassengers, key=lambda passenger: passenger.departureTime)
     # print('listAfter: ', sortedPassengers)
@@ -327,10 +328,23 @@ def digital_colony():
     results = calculateWeightOverGeneration(data)
     return json.dumps(results)
 
+@app.route('/chinese-wall', methods=['GET'])
+def chinese_wall():
+  listPassword = {
+    "1": "Fluffy",
+    "2": "Galactic",
+    "3": "Mangoes",
+    "4": "Subatomic",
+    "5": "Guitar"
+  }
+
+  return jsonify(listPassword)
+  
 
 @app.route('/airport', methods=['POST'])
 def airport_checkin():
     data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
 
     results = []
     for item in data:
@@ -377,3 +391,56 @@ def move(data):
           }
 
     return {"playerAction": "stay"}
+
+@app.route('/pie-chart', methods=['POST'])
+def calculate_pie_chart():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    results = processCalculationRadians(data['data'])
+    return jsonify(results)
+
+def calculate_radians(portfolio):
+    total_investment = sum(portfolio)
+    proportions = [investment / total_investment for investment in portfolio]
+
+    min_proportion_radians = 0.00314159  # Minimum radians for instruments with small proportions
+    min_proportion = 0.05/100
+    total_radians = 2 * math.pi
+
+    radians = []
+    total_adjustment_diff = 0
+    total_unadjusted_investment = 0
+    for proportion in proportions:
+        if proportion < min_proportion:
+            radians.append(min_proportion_radians)
+            total_adjustment_diff += (min_proportion_radians-proportion*total_radians)
+        else:
+            radians.append(proportion * total_radians)
+            total_unadjusted_investment += (proportion * total_radians)
+
+    #radians = [min_proportion_radians if proportion < min_proportion else proportion * total_radians for proportion in proportions]
+    
+     # Calculate radians for each instrument, adjusting for small proportions
+    if(total_adjustment_diff):
+        for radian in radians:
+            if(radian > min_proportion_radians):
+                radian -= (total_adjustment_diff * radian/total_unadjusted_investment)
+
+    # Sort instruments and calculate boundaries
+    #sorted_instruments = sorted(zip(portfolio, radians), key=lambda x: x[0]['investment'], reverse=True)
+    radians.sort(reverse=True)
+    boundaries = [0.0]
+    accumulated_radians = 0.0
+
+    for radian in radians:
+        accumulated_radians += radian
+        boundaries.append(round(accumulated_radians, 7))
+
+    return boundaries
+
+def processCalculationRadians(portfolio):
+    listInvestmentAmount = [instrument['quantity']*instrument['price'] for instrument in portfolio]
+    radianResult = calculate_radians(listInvestmentAmount)
+    return {
+        "instruments": radianResult
+    }
