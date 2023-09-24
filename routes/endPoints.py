119d2e21daedc10041f5035299d06ae56755d2d8
Vedika Saini <vedikasaini10@gmail.com>
2023-09-24 09:02:53 +0800

    gm final

--- routes/endPoints.py
+++ routes/endPoints.py
@@ -72,23 +72,19 @@ def evaluateGreedyMonkey():
 
 def calculate_maximum_value(weight_limit, value_limit, items):
     memo = {}
-
-    def dynamic_programming(curr_weight, curr_value, index):
+    def dp(curr_weight, curr_value, index):
         if index == len(items):
             return 0
-
         if (curr_weight, curr_value, index) in memo:
             return memo[(curr_weight, curr_value, index)]
-
         max_value = 0
         if curr_weight + items[index][0] <= weight_limit and curr_value + items[index][1] <= value_limit:
-            max_value = max(max_value, items[index][2] + dynamic_programming(curr_weight + items[index][0], curr_value + items[index][1], index + 1))
-        max_value = max(max_value, dynamic_programming(curr_weight, curr_value, index + 1))
+            max_value = max(max_value, items[index][2] + dp(curr_weight + items[index][0], curr_value + items[index][1], index + 1))
+        max_value = max(max_value, dp(curr_weight, curr_value, index + 1))
 
         memo[(curr_weight, curr_value, index)] = max_value
         return max_value
-
-    return dynamic_programming(0, 0, 0)
+    return dp(0, 0, 0)
 
 @app.route("/railway-builder", methods=['POST'])
 def railway_builder():
